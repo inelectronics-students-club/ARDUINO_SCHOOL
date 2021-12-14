fe3da9aa6c51e83bf1a4fddb40614377ee7df0c6
inelectronics-students-club <94116296+inelectronics-students-club@users.noreply.github.com>
2021-12-14 20:27:58 +0100

    Add files via upload

---
+++ ARDUINO SCHOOL 2/session2/sketch2/sketch2.ino
@@ -0,0 +1,15 @@
+int smokeA0 = A0;
+
+
+void setup() {
+  pinMode(smokeA0, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  int analogSensor = analogRead(smokeA0);
+
+  Serial.print("Pin A0: ");
+  Serial.println(analogSensor);
+  
+}
